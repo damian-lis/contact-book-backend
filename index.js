b1dc49406f2d46b16002ff7a62f6d4b0888e0adf
Damian Lis <foxprogramming93@gmail.com>
2021-07-09 10:07:14 +0200

    Corrected the structure of index.js file

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const cors = require('cors');
 connectDB();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use('/contact', contactRoutes);
 app.use('/auth', authRoutes);
 
-const PORT = 5000
-
 app.listen(
-  process.env.PORT || PORT,
-  console.log(`Server is running on ${process.env.PORT || PORT}`)
+ PORT,
+  console.log(`Server is running on ${PORT}`)
 );
