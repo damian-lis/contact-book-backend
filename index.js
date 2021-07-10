a6296d0925312ceafd758f91cf1c7151ec700ec4
Damian Lis <foxprogramming93@gmail.com>
2021-07-10 13:49:53 +0200

    Created simple title page

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
+app.use(express.static('public'))
 app.use(express.json({ limit: '50mb' }));
 app.use('/contact', contactRoutes);
 app.use('/auth', authRoutes);
